c7eb52799e55712f0b03abca570bdff0f89ac2b0
malabasterr <113557081+malabasterr@users.noreply.github.com>
2025-05-29 16:07:45 +0100

    add router and example rules page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MapComponent from "./components/MapComponent";
+import RulesPage from './pages/RulesPage';
 
 function App() {
 
   return (
-    <>
+    <div>
+    <Router>
+      <div className="appContainer">
+        <Routes>
+          <Route path="/rules" element={<RulesPage />} />
+        </Routes>
+      </div>
+    </Router>
       <h1>All Change: The Game</h1>
       <p className="paragraph">
         Here is some sample text!
@@ -12,8 +21,57 @@ function App() {
       <div className="mapContainer">
         <MapComponent />
       </div>
-    </>
+      </div>
   )
 }
 
 export default App
+
+
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import FooterComponent from './components/footer/FooterComponent';
+// import ClaimRoutePage from './pages/forms/ClaimRoutePage';
+// import UnclaimRoutePage from './pages/forms/UnclaimRoutePage';
+// import ClaimBonusSitePage from './pages/forms/ClaimBonusSitePage';
+// import ShareLocationPage from './pages/forms/ShareLocationPage';
+// import DrawScrewYouCardPage from './pages/drawScrewYou/DrawScrewYouCardPage';
+// import Error from './pages/Error';
+// import Login from './aws/Login';
+// import Home from './aws/Home';
+// import store from './redux/store';
+// import { Provider } from "react-redux";
+// import MWDistanceSummary from './components/summaries/MWDistanceSummary';
+// import JHDistanceSummary from './components/summaries/JHDistanceSummary';
+// import WMDistanceSummary from './components/summaries/WMDistanceSummary';
+// import RulesPage from './components/RulesPage';
+
+// function App() {
+//   return (
+//     <Provider store={store}>
+//     <Router>
+//       <div className="appContainer">
+//         <Routes>
+//           <Route path="/" element={<Login />} />
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/claimroute" element={<ClaimRoutePage />} />
+//           <Route path="/unclaimroute" element={<UnclaimRoutePage />} />
+//           <Route path="/claimbonussite" element={<ClaimBonusSitePage />} />
+//           <Route path="/sharelocation" element={<ShareLocationPage />} />
+//           <Route path="/drawscrewyoucard" element={<DrawScrewYouCardPage />} />
+//           <Route path="/MWSummary" element={<MWDistanceSummary />} />
+//           <Route path="/JHSummary" element={<JHDistanceSummary />} />
+//           <Route path="/WMSummary" element={<WMDistanceSummary />} />
+//           <Route path="/rules" element={<RulesPage />} />
+//           <Route path="*" element={<Error />} />
+//         </Routes>
+//         <FooterComponent />
+//       </div>
+//     </Router>
+//     </Provider>
+//   );
+// }
+
+// export default App;
